614fa231f20530a151b9bf90e4c5b8c675b001fa
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2021-07-12 20:53:54 -0300

    initial pokemon request with axios

--- src/components/RightScreen.js
+++ src/components/RightScreen.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from 'axios';
+// import Pokemon from "./Pokemon"
 
 const RightScreen = () => {
+  const [pokemonName, setPokemonName] = useState("pikachu");
+
+  const [pokemon, setPokemon] = useState([
+    { category: '---', type: ['---'], height: '---', weight: '---', evolution: '---', abilities: ['---'] }
+  ]);
+
+  const seachPokemon = async () => {
+    const response = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+    // type pode ter varias posições
+    // abilities podem ter várias posições
+    setPokemon({category: '---', type: ['---'], height: response.data.height, weight: response.data.weight, evolution: '---', abilities: [ '---' ]});
+
+  }
+
   return (
     <div className="right-screen">
       <div className="right-screen-top">
@@ -15,8 +32,11 @@ const RightScreen = () => {
               className="info-input"
               placeholder="Busque um pokemon"
               defaultValue="pikachu"
+              onChange={(event) => {
+                setPokemonName(event.target.value)
+              }}
             />
-            <button className="info-button" onclick="requestAPI()">
+            <button className="info-button" onClick={seachPokemon}>
               Buscar
             </button>
           </div>
@@ -36,13 +56,13 @@ const RightScreen = () => {
             <div id="tamanho" className="info">
               <div className="label">Tamanho:</div>
               <div className="desc" id="info-text">
-                - - -
+                {pokemon.height}
               </div>
             </div>
             <div id="peso" className="info">
               <div className="label">Peso:</div>
               <div className="desc" id="info-text">
-                - - -
+              {pokemon.weight}
               </div>
             </div>
             <div id="evolution" className="info">
